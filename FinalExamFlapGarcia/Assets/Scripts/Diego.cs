b039352fb6fd5e6a2b9864ccb6ca235f5985c40b
Xx-Diego-XX-Agustin-xx-Garcia-xX <720791@student.dusd.net>
2022-12-15 15:25:52 -0800

    Final Exam Animations
    E

--- FinalExamFlapGarcia/Assets/Scripts/Diego.cs
+++ FinalExamFlapGarcia/Assets/Scripts/Diego.cs
@@ -1,29 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Diego : MonoBehaviour
 {
     private bool isDead = false;
     private Rigidbody2D rb2d;
-    public float upForce = 196f;
+    public float upForce;
+    private Animator anim;
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
     void Update()
     {
         if (isDead == false)
         {
-            if (Input.GetMouseButtonUp(0))
+            if (Input.GetMouseButtonDown(0))
             {
                 rb2d.velocity = Vector2.zero;
                 rb2d.AddForce(new Vector2(0, upForce));
+                anim.SetTrigger("Flap");
             }
         }
     }
     private void OnCollisionEnter2D()
     {
         isDead = true;
+        anim.SetTrigger("Die");
     }
 }
